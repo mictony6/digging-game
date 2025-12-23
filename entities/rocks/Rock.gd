262e5ce4dd3f93019cb3f6f5a1a77b112fa4655b
Michael Anthony Bitoon <mbitoon@up.edu.ph>
2025-12-24 01:50:29 +0800

    -add some textures - added rock breaking crack shader progress animation - added shader for non tiling ground texture with two possible textures configurable

--- entities/rocks/Rock.gd
+++ entities/rocks/Rock.gd
@@ -4,7 +4,7 @@ class_name Rock
 @export var rock_data: RockData
 var current_health: int
 signal destroyed(body: Rock)
-
+var crack_material: ShaderMaterial
 
 func _ready():
 	current_health = rock_data.max_health
@@ -21,16 +21,27 @@ func _ready():
 		i.visible = false
 	var mesh_index = randi() % $MeshTypes.get_child_count()
 	$MeshTypes.get_child(mesh_index).visible = true
-
+	crack_material = $MeshTypes.get_child(mesh_index).material_overlay
 
 func take_damage(damage: int):
+	if current_health <= 0:
+		destroy()
 	if not rock_data.is_destructible:
 		return
 	current_health -= damage
-	if current_health <= 0:
-		destroy()
+
 
 func destroy():
 	QuotaManager.add_to_quota(rock_data.value)
 	destroyed.emit(self)
 	queue_free()
+
+func _process(delta: float) -> void:
+	#update crack shader based on health
+	if current_health == rock_data.max_health:
+		return
+	if rock_data.is_destructible:
+		var health_ratio = 1.0 - float(current_health) / float(rock_data.max_health)
+		var current_ratio = crack_material.get_shader_parameter("ratio")
+		health_ratio = lerpf(current_ratio, health_ratio, 0.1)
+		crack_material.set_shader_parameter("ratio", health_ratio)
